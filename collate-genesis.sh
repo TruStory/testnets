fc2e0e3cdee54bb831b41eec976c1ffc3a784335
Shane Vitarana <shanev@gmail.com>
2019-11-21 20:28:12 -0800

    Added collate genesis script

---
+++ collate-genesis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NETWORK=testnet-1
+CONFIG=~/.truchaind
+
+rm -rf $CONFIG/gentx && mkdir $CONFIG/gentx
+
+for i in $NETWORK/gentx/*.json; do
+  echo $i
+  truchaind add-genesis-account $(jq -r '.value.msg[0].value.delegator_address' $i) $(jq -r '.value.msg[0].value.value.amount' $i)$(jq -r '.value.msg[0].value.value.denom' $i)
+  cp $i $CONFIG/gentx/
+done
+truchaind collect-gentxs
+
+cp $CONFIG/genesis.json $NETWORK
