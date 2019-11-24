eaddf42f25a532d56ae0717089298e9ce991d077
Shane Vitarana <shanev@gmail.com>
2019-11-23 19:12:35 -0800

    Updated format script

--- format-gentxs.sh
+++ format-gentxs.sh
@@ -4,7 +4,7 @@ NETWORK=testnet-1
 
 for i in $NETWORK/gentx/*.json; do
   echo $i
-  cat $i | jq > /tmp/gentx.json
+  cat $i | jq '.' > /tmp/gentx.json
   cp /tmp/gentx.json $i
   rm /tmp/gentx.json
 done
